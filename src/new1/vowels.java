e482a1f54cf7e56291b4c8a07908a54f6c10349a
unknown <adarshkps1412@gmail.com>
2025-09-17 20:50:11 +0530

    mahesh

--- src/new1/vowels.java
+++ src/new1/vowels.java
@@ -4,7 +4,7 @@ public class vowels {
 
 	
 	public static void main(String[] args) {
-		String str="nikil";
+		String str="mahesh";
 		if (str.matches(".*[aeiou].*")) {
 		    System.out.println("good work");
 		}
