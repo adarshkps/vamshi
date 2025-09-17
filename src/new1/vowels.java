dd42a5c7f27d10ae0d113373361311326666424b
unknown <adarshkps1412@gmail.com>
2025-09-17 20:22:20 +0530

    vamshi day 1

---
+++ src/new1/vowels.java
@@ -0,0 +1,13 @@
+package new1;
+
+public class vowels {
+
+	
+	public static void main(String[] args) {
+		String str="adarsh";
+		if (str.matches(".*[aeiou].*")) {
+		    System.out.println("good work");
+		}
+	}
+
+}
